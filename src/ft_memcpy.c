775e82e5b0dcde4ac13314e2037c0b1c2422cd65
Pavit Pimchanagul <ppimchan@z3t1c1.42bangkok.com>
2022-03-14 11:07:22 +0700

    refract

--- src/ft_memcpy.c
+++ src/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: ppimchan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/01 02:34:51 by ppimchan          #+#    #+#             */
-/*   Updated: 2022/03/06 21:52:17 by ppimchan         ###   ########.fr       */
+/*   Updated: 2022/03/14 11:04:24 by ppimchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,12 @@
 
 void	*ft_memcpy(void	*dst, const void *src, size_t n)
 {
-	char	*content_dst = (char *)dst;
-	char	*content_src = (char *)src;
-	int	i;
-	
+	char	*content_dst;
+	char	*content_src;
+	int		i;
+
+	*content_dst = (char *)dst;
+	*content_src = (char *)src;
 	i = 0;
 	while (i < n)
 	{
@@ -28,23 +30,19 @@ void	*ft_memcpy(void	*dst, const void *src, size_t n)
 	}
 	return (dst);
 }
-
-int	main (void)
-{
-	char str[100] = "Learningisfun";
-	char str_2[100] = "Learningisfun";
-	char *first, *second;
-	first = str;
-	second = str;
-	printf("Original string :%s\n ", str);
-			      
+//int	main (void)
+//{
+//	char str[100] = "Learningisfun";
+//	char str_2[100] = "Learningisfun";
+//	char *first, *second;
+//	first = str;
+//	second = str;
+//	printf("Original string :%s\n ", str);
 	//when overlap happens then it just ignore it
-	memcpy(first + 8, str, 10);
-	printf("memcpy overlap : %s\n ", str);
-			               
+//	memcpy(first + 8, str, 10);
+//	printf("memcpy overlap : %s\n ", str);			               
 	// my memcpy overlap
-	ft_memcpy(first + 8, str, 10);
-	printf("my memcpy overlap : %s\n ", str);
-			                             
-	return 0;
-}
+//	ft_memcpy(first + 8, str, 10);
+//	printf("my memcpy overlap : %s\n ", str);
+//	return 0;
+//}
