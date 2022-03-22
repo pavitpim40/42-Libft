0a0c03cc20a52b75dac5a46d8d4621a057481728
pavitpim40 <pavitpim@gmail.com>
2022-03-22 09:15:18 +0700

    test alter version of ft_strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -12,28 +12,61 @@
 
 #include "libft.h"
 
+
+static	int	check_set(char const c, char const *set);
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	len;
-	size_t	i;
-	size_t	j;
-	char	*res;
+	int		i;
+	int		len;
+	char	*str;
 
-	i = 0;
 	len = ft_strlen(s1);
-	while (len > 0 && ft_strchr(set,s1[len - 1]) != NULL)
+	while (check_set(*(s1 + len - 1), set) && len > 0)
 		len--;
-	while (len > 0 && ft_strchr(set,s1[i++]) != NULL)
+	while (check_set(*s1++, set) && len > 0)
 		len--;
-	i -= 1;
-	res = ft_calloc(len + 1,1);
-	if (!res)
-		return(NULL);
-	j = 0;
-	while (j < len)
-		res[j++] = s1[i++];  
-	res[j] = '\0';
-	return (res);	
+	s1--;
+	i = 0;
+	str = malloc(sizeof(char) * len);
+	while (i < len)
+		str[i++] = *s1++;
+	str[i] = '\0';
+	return (str);
 }
+
+static	int	check_set(char const c, char const *set)
+{
+	while (*set)
+		if (c == *set++)
+			return (1);
+	return (0);
+}
+
+
+
+// char	*ft_strtrim(char const *s1, char const *set)
+// {
+// 	size_t	len;
+// 	size_t	i;
+// 	size_t	j;
+// 	char	*res;
+
+// 	i = 0;
+// 	len = ft_strlen(s1);
+// 	while (len > 0 && ft_strchr(set,s1[len - 1]) != NULL)
+// 		len--;
+// 	while (len > 0 && ft_strchr(set,s1[i++]) != NULL)
+// 		len--;
+// 	i -= 1;
+// 	res = ft_calloc(len + 1,1);
+// 	if (!res)
+// 		return(NULL);
+// 	j = 0;
+// 	while (j < len)
+// 		res[j++] = s1[i++];  
+// 	res[j] = '\0';
+// 	return (res);	
+// }
 
 
