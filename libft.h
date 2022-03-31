8998fb5cc92012519496640225942696405ba06c
pavitpim40 <pavitpim@gmail.com>
2022-04-01 00:30:01 +0700

    add ft_putchar_fd

--- libft.h
+++ libft.h
@@ -33,10 +33,14 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_itoa(int n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 char    **ft_split(char const *s, char c);
 
-void	*ft_memset(void *b, int c, size_t len);
+
 void	ft_bzero(void *s, size_t n);
+void	ft_putchar_fd(char	c,int	fd);
+void	*ft_memset(void *b, int c, size_t len);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t n);
 void	*ft_calloc(size_t count, size_t size);
