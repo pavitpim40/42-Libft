018261461be5dce8757045419145b1d872b6806c
pavitpim40 <pavitpim@gmail.com>
2022-03-31 08:15:12 +0700

    finist itoa

--- dev/ft_itoa/ft_itoa.c
+++ dev/ft_itoa/ft_itoa.c
@@ -10,26 +10,46 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-static size_t ft_intlen (size_t nb)
-{
-	size_t len;
-	size_t i;
+#include <stdio.h>
+#include <stdlib.h>
 
-	len = 0;
-	i = nb;
-	if (nb < 0)
+static void	ft_bzero(void *s, size_t n)
+{
+	char	*str;
+	size_t	i;
+
+	i = 0;
+	str = (char *)s;
+	while (i < n)
 	{
-		len++;
-		i = -nb;
+		str[i] = '\0';
+		i++;
 	}
-	while (i > 0)
-	{
-		i = i / 10;
-		len++;
-	}
-	return (len);
 }
+
+
+static void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ptr;
+
+	ptr = malloc(count * size);
+	if (ptr == 0)
+		return (ptr);
+	ft_bzero(ptr, count * size);
+	return (ptr);
+}
+static int ft_intlen (long nb)
+{
+	int	size;
+	size = 0;
+	while (nb >= 10)
+	{
+		nb /= 10;
+		++size;
+	}
+	return (size + 1);
+}
+
 
 static int ft_power (int nb, int power)
 {
@@ -49,7 +69,7 @@ static int ft_power (int nb, int power)
 char	*ft_itoa(int nb)
 {
 	char	*str;
-	int 	len;
+	int len;
 	size_t	i;
 	int		sign;
 
@@ -74,4 +94,12 @@ char	*ft_itoa(int nb)
 		nb = nb % ft_power(10, len);
 	}
 	return (str);
+}
+
+int main ()
+{
+
+	// printf("## %u\n", ft_intlen(-214));
+	printf("%s", ft_itoa(-526));
+	return (0);
 }
