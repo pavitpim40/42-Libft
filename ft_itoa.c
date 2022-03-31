b93bf630cc8d534e5944eac25bed9640d9035fe5
Pavit Pimchanagul <ppimchan@z3t1c1.42bangkok.com>
2022-03-31 11:25:36 +0700

    refractor ft_itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,14 +6,16 @@
 /*   By: ppimchan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/27 00:07:47 by ppimchan          #+#    #+#             */
-/*   Updated: 2022/03/27 00:07:49 by ppimchan         ###   ########.fr       */
+/*   Updated: 2022/03/31 11:25:01 by ppimchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-static int ft_intlen (long nb)
+
+static int	ft_intlen(long nb)
 {
 	int	size;
+
 	size = 0;
 	while (nb >= 10)
 	{
@@ -23,10 +25,10 @@ static int ft_intlen (long nb)
 	return (size + 1);
 }
 
-static int ft_power (int nb, int power)
+static int	ft_power(int nb, int power)
 {
-	int i;
-	int result;
+	int	i;
+	int	result;
 
 	i = 0;
 	result = 1;
@@ -38,32 +40,42 @@ static int ft_power (int nb, int power)
 	return (result);
 }
 
+static char	*ft_write(char *str, int nbr, int sign, int len)
+{
+	size_t	i;
+
+	i = 0;
+	if (nbr < 0)
+	{
+		str[i++] = '-';
+		len-- ;
+	}
+	while (len--)
+	{
+		str[i++] = (nbr * sign / ft_power(10, len)) + '0';
+		nbr = nbr % ft_power(10, len);
+	}
+	return (str);
+}
+
 char	*ft_itoa(int nb)
 {
 	char	*str;
-	int 	len;
-	size_t	i;
+	int		len;
 	int		sign;
+	long	nbr;
 
 	len = 0;
-	i = 0;
 	sign = 1;
 	if (nb < 0)
 	{
 		sign = -1;
 		len = 1;
 	}
-	len += ft_intlen((long)nb*sign);
-	if (!(str = ft_calloc(len + 1, sizeof(char))))
+	nbr = nb * sign;
+	len += ft_intlen (nbr);
+	str = ft_calloc(len + 1, sizeof(char));
+	if (!str)
 		return (NULL);
-	if (nb < 0){
-		str[i++] = '-';
-		len-- ;
-	}
-	while (len--)
-	{
-		str[i++] = ((long)nb*sign / ft_power(10, len)) + '0';
-		nb = nb % ft_power(10, len);
-	}
-	return (str);
+	return (ft_write(str, nb, sign, len));
 }
