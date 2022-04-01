cfbb2d2be812040a335d9ba5ac4ef90cc6cce399
Pavit <pavitpim@gmail.com>
2022-04-01 11:22:36 +0700

    add ft_putstr_fd

--- libft.h
+++ libft.h
@@ -39,7 +39,8 @@ char    **ft_split(char const *s, char c);
 
 
 void	ft_bzero(void *s, size_t n);
-void	ft_putchar_fd(char	c,int	fd);
+void	ft_putchar_fd(char c,int fd);
+void    ft_putstr_fd(char *s, int fd);
 void	*ft_memset(void *b, int c, size_t len);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t n);
