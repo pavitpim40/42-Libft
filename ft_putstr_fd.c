9ca827915bccd4e533b3a8f0af4c8b18f546d1c8
Pavit Pimchanagul <ppimchan@z3t1c1.42bangkok.com>
2022-04-01 11:28:54 +0700

    norm ft_putstr_fd

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -1,9 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ppimchan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/01 11:26:39 by ppimchan          #+#    #+#             */
+/*   Updated: 2022/04/01 11:28:28 by ppimchan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 
-void    ft_putstr_fd(char *s, int fd)
+void	ft_putstr_fd(char *s, int fd)
 {
-    if (s == NULL)
-        return ;
-    write (fd, s, ft_strlen(s));
+	if (s == NULL)
+		return ;
+	write (fd, s, ft_strlen(s));
 }
