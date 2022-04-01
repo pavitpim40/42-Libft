84998c804ec9bc67e15d6af834adc71abbbeaf5b
Pavit <pavitpim@gmail.com>
2022-04-01 11:44:19 +0700

    update lib and makefile

--- libft.h
+++ libft.h
@@ -21,6 +21,9 @@ int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+int		ft_atoi(const char *str);
 
 size_t	ft_strlen(const char *str);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
@@ -39,13 +42,14 @@ char    **ft_split(char const *s, char c);
 
 
 void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t count, size_t size);
+
 void	ft_putchar_fd(char c,int fd);
 void    ft_putstr_fd(char *s, int fd);
+void    ft_putendl_fd(char *s, int fd);
+
 void	*ft_memset(void *b, int c, size_t len);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t n);
-void	*ft_calloc(size_t count, size_t size);
-int		ft_toupper(int c);
-int		ft_tolower(int c);
-int		ft_atoi(const char *str);
+
 #endif
