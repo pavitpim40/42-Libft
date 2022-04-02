c7befbad9282b943d70a3c241b4e84998fc6604f
pavitpim40 <pavitpim@gmail.com>
2022-04-03 00:44:11 +0700

    add ft_lstadd_back

--- libft.h
+++ libft.h
@@ -61,7 +61,9 @@ void	*ft_memmove(void *dst, const void *src, size_t n);
 
 t_list	*ft_lstnew(void *content);
 void    ft_lstadd_front(t_list **lst, t_list *new);
+void    ft_lstadd_back(t_list **lst, t_list *new);
 int     ft_lstsize(t_list *lst);
 t_list  *ft_lstlast(t_list *lst);
+
 
 #endif
