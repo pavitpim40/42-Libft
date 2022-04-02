a49fb5e41764ca2e67b91cfb8c25940dab5f9a68
pavitpim40 <pavitpim@gmail.com>
2022-04-03 00:57:23 +0700

    add ft_lstadd_back

---
+++ ft_lstadd_back.c
@@ -0,0 +1,17 @@
+
+#include "libft.h"
+
+void ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list *tmp;
+
+	if (*lst == NULL)
+		*lst = new;
+	else
+	{
+		tmp = *lst;
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+		tmp->next = new;
+	}
+}
