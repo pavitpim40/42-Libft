d2d1e0d8dcda690da5c68bf9fdc99c6680fe736f
pavitpim40 <pavitpim@gmail.com>
2022-04-15 10:21:36 +0700

    fixed calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -16,7 +16,7 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
 
-	if (size > 65535)
+	if (count * size < count && count * size < size)
 		return (NULL);
 	ptr = malloc(count * size);
 	if (ptr == 0)
